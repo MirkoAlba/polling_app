0b95e73df30b8feff88988ec5c1815d6306c2139
Mirko Albanese <mirkoalba006@gmail.com>
2021-07-18 16:32:38 +0200

    mod footer style

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,34 +1,36 @@
 import { Container, Row, Col } from "react-bootstrap";
 export default function Footer() {
   return (
-    <Container className="footer">
-      <Row>
-        <Col md={3}>Logo</Col>
-        <Col md={3} className="mt-3 mt-md-0">
-          <ul>
-            ciao
-            <li className="mt-2">ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-          </ul>
-        </Col>
-        <Col md={3}>
-          <ul>
-            ciao
-            <li className="mt-2">ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-          </ul>
-        </Col>
-        <Col md={3}>
-          <ul>
-            ciao
-            <li className="mt-2">ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
+    <div className="wrapper-footer">
+      <Container className="footer pt-4 pb-2">
+        <Row>
+          <Col md={3}>Logo</Col>
+          <Col md={3} className="mt-3 mt-md-0">
+            <ul>
+              <strong>ciao</strong>
+              <li className="mt-2">ciao</li>
+              <li>ciao</li>
+              <li>ciao</li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <ul>
+              <strong>ciao</strong>
+              <li className="mt-2">ciao</li>
+              <li>ciao</li>
+              <li>ciao</li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <ul>
+              <strong>ciao</strong>
+              <li className="mt-2">ciao</li>
+              <li>ciao</li>
+              <li>ciao</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
